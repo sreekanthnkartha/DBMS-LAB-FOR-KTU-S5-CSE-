2351a29fa0ae559c317c716f87585b446a7ced15
AthulJohn <athulpulickaljohn@gmail.com>
2022-10-21 19:14:58 +0530

    Update bank.sql
    Added explanatory comments for better readability.
    Removed unused variables.

--- sql/bank.sql
+++ sql/bank.sql
@@ -1,16 +1,16 @@
 set serveroutput on
 accept x char prompt '1.Debit   2.Credit   3.Check balance : '
 declare
-    acc_no acc.accno%type;
-    balance acc.bal%type;
-    choice char;
-    choice2 char;
-    choice3 char;
-    amount int;
+    acc_no acc.accno%type; -- variable for inputing the account no to which the operations should be done. 
+    balance acc.bal%type; -- variable for storing the current balance of the account. It is fetched from the database.
+    choice char; -- variable for inputing the choice of the user. 
+    amount int; -- variable for storing amount, which should be debited or credited.
 begin
+ -- Fetching data from user : 
     choice := &x;
     acc_no := &acc_no;
     amount := &amount;
+ -- Fetching the current balance from the database. (according to the account number)
     select bal into balance from acc where accno = acc_no;
     case
         when choice = '1' then
